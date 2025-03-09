cec3311f9980f97127c70f1a498f02d52d3e9265
erin <erin@techtaka.com>
2025-03-09 19:34:03 +0900

    dark mode

---
+++ colors.ts
@@ -0,0 +1,16 @@
+export const COLORS = {
+  // Light Mode
+  light: {
+    PRIMARY: '#ffffff', // 주 배경색
+    TEXT: '#1e272e', // 주 텍스트 색상
+    INACTIVE_TEXT: '#808e9b', // 비활성 텍스트
+    ACTIVE: '#000000', // 활성 상태 색상
+  },
+  // Dark Mode
+  dark: {
+    PRIMARY: '#1e272e', // 주 배경색
+    TEXT: '#ffffff', // 주 텍스트 색상
+    INACTIVE_TEXT: '#808e9b', // 비활성 텍스트
+    ACTIVE: '#ffffff', // 활성 상태 색상
+  },
+};
--- navigation/Tabs.tsx
+++ navigation/Tabs.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {useColorScheme} from 'react-native';
 import Movies from '../screens/Movies';
 import Tv from '../screens/Tv';
 import Search from '../screens/Search';
+import {COLORS} from '../colors';
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movies} />
-    <Tab.Screen name="Tv" component={Tv} />
-    <Tab.Screen name="Search" component={Search} />
-  </Tab.Navigator>
-);
+const Tabs = () => {
+  const isDark = useColorScheme() === 'dark';
+  const colors = isDark ? COLORS.dark : COLORS.light;
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: colors.PRIMARY,
+        },
+        tabBarActiveTintColor: colors.ACTIVE,
+        tabBarInactiveTintColor: colors.INACTIVE_TEXT,
+        headerStyle: {
+          backgroundColor: colors.PRIMARY,
+        },
+        headerTitleStyle: {
+          color: colors.TEXT,
+        },
+      }}>
+      <Tab.Screen name="Movies" component={Movies} />
+      <Tab.Screen name="Tv" component={Tv} />
+      <Tab.Screen name="Search" component={Search} />
+    </Tab.Navigator>
+  );
+};
 
 export default Tabs;
