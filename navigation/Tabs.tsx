c7fdec8e5febf4fc533b4c6a1d9246d18c8f8ec3
erin <erin@techtaka.com>
2025-03-09 20:04:21 +0900

    Tab Bar Icons

--- navigation/Tabs.tsx
+++ navigation/Tabs.tsx
@@ -5,6 +5,7 @@ import Movies from '../screens/Movies';
 import Tv from '../screens/Tv';
 import Search from '../screens/Search';
 import {COLORS} from '../colors';
+import {Ionicons} from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -17,6 +18,7 @@ const Tabs = () => {
       screenOptions={{
         tabBarStyle: {
           backgroundColor: colors.PRIMARY,
+          height: 80,
         },
         tabBarActiveTintColor: colors.ACTIVE,
         tabBarInactiveTintColor: colors.INACTIVE_TEXT,
@@ -26,10 +28,42 @@ const Tabs = () => {
         headerTitleStyle: {
           color: colors.TEXT,
         },
+        tabBarLabelStyle: {
+          fontSize: 10,
+          fontWeight: '600',
+          marginTop: -4,
+        },
+        tabBarIconStyle: {
+          marginTop: 10,
+        },
       }}>
-      <Tab.Screen name="Movies" component={Movies} />
-      <Tab.Screen name="Tv" component={Tv} />
-      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen
+        name="Movies"
+        component={Movies}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name={'film-outline'} color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="TV"
+        component={Tv}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="tv-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name={'search-outline'} color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
