618a014cb59c6f0ce5753fc6cfa31dc1fa093b8c
erin <erin@techtaka.com>
2025-03-10 21:30:30 +0900

    styled component

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,6 +1,7 @@
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
+import styled from 'styled-components/native';
 
 type RootStackParamList = {
   Stack: {screen: string};
@@ -10,11 +11,21 @@ type Props = {
   navigation: NativeStackNavigationProp<RootStackParamList>;
 };
 
+const Button = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: blue;
+  font-size: 20px;
+  font-weight: bold;
+`;
+
 const Movies = ({navigation: {navigate}}: Props) => (
-  <TouchableOpacity
-    onPress={() => navigate('Stack', {screen: 'Three'})}
-    style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text>Movies</Text>
-  </TouchableOpacity>
+  <Button onPress={() => navigate('Stack', {screen: 'Three'})}>
+    <Title>Movies</Title>
+  </Button>
 );
 export default Movies;
