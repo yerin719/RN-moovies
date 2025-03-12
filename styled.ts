cd685b2e7108e84644ebc94864f601bc02724996
erin <erin@techtaka.com>
2025-03-12 21:42:11 +0900

    type error

--- styled.ts
+++ styled.ts
@@ -1,8 +1,13 @@
-export const lightTheme = {
+import {DefaultTheme} from 'styled-components/native';
+
+const lightTheme: DefaultTheme = {
   mainBgColor: 'white',
   textColor: '#1e272e',
 };
-export const darkTheme = {
+
+const darkTheme: DefaultTheme = {
   mainBgColor: '#1e272e',
   textColor: '#d2dae2',
 };
+
+export {lightTheme, darkTheme};
