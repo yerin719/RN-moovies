d816d076fd1462b01f210d305bfecbc9458baa2b
erin <erin@techtaka.com>
2025-03-12 22:44:30 +0900

    Movies Screen

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,26 +1,85 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, Dimensions} from 'react-native';
+import Swiper from 'react-native-swiper';
 import styled from 'styled-components/native';
+import Slide from '../components/Slide';
 
-const Button = styled.TouchableOpacity`
+const API_KEY = '057de6dbeabba265ae577423ef50fc4e';
+
+const Container = styled.ScrollView``;
+
+const Loader = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: ${props => props.theme.mainBgColor};
 `;
 
-const Title = styled.Text`
-  color: blue;
-  font-size: 20px;
-  font-weight: bold;
-  color: ${props => props.theme.textColor};
-`;
+const {height: SCREEN_HEIGHT} = Dimensions.get('window');
 
-const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = ({
-  navigation: {navigate},
-}) => (
-  <Button onPress={() => navigate('Stack', {screen: 'Three'})}>
-    <Title>Movies</Title>
-  </Button>
-);
+const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
+  const [loading, setLoading] = useState(true);
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+  const [trending, setTrending] = useState([]);
+  const getTrending = async () => {
+    const {results} = await (
+      await fetch(
+        `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`,
+      )
+    ).json();
+    setTrending(results);
+  };
+  const getUpcoming = async () => {
+    const {results} = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`,
+      )
+    ).json();
+    setUpcoming(results);
+  };
+  const getNowPlaying = async () => {
+    const {results} = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`,
+      )
+    ).json();
+    setNowPlaying(results);
+  };
+  const getData = async () => {
+    await Promise.all([getTrending(), getUpcoming(), getNowPlaying()]);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return loading ? (
+    <Loader>
+      <ActivityIndicator />
+    </Loader>
+  ) : (
+    <Container>
+      <Swiper
+        horizontal
+        loop
+        autoplay
+        autoplayTimeout={3.5}
+        showsButtons={false}
+        showsPagination={false}
+        containerStyle={{width: '100%', height: SCREEN_HEIGHT / 4}}>
+        {nowPlaying.map(movie => (
+          <Slide
+            key={movie.id}
+            backdropPath={movie.backdrop_path}
+            posterPath={movie.poster_path}
+            originalTitle={movie.original_title}
+            voteAverage={movie.vote_average}
+            overview={movie.overview}
+          />
+        ))}
+      </Swiper>
+    </Container>
+  );
+};
+
 export default Movies;
