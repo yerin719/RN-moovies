5728293377e0cc722ce91a4a89ef18fb6f47e4be
erin <erin@techtaka.com>
2025-03-16 21:17:22 +0900

    Search screen

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,22 +1,65 @@
-import React from 'react';
-import {View, Text, useColorScheme} from 'react-native';
-import {COLORS} from '../colors';
+import React, {useState} from 'react';
+import {Alert} from 'react-native';
+import {useQuery} from '@tanstack/react-query';
+import styled from 'styled-components/native';
+import {moviesApi, tvApi} from '../api';
+import HList from '../components/HList';
+import Loader from '../components/Loader';
+
+const Container = styled.ScrollView``;
+
+const SearchBar = styled.TextInput`
+  background-color: white;
+  padding: 10px 15px;
+  border-radius: 15px;
+  width: 90%;
+  margin: 10px auto;
+  margin-bottom: 40px;
+`;
 
 const Search = () => {
-  const isDark = useColorScheme() === 'dark';
-  const colors = isDark ? COLORS.dark : COLORS.light;
-
+  const [query, setQuery] = useState('');
+  const {
+    isLoading: moviesLoading,
+    data: moviesData,
+    refetch: searchMovies,
+  } = useQuery({
+    queryKey: ['searchMovies', query],
+    queryFn: moviesApi.search,
+    enabled: false,
+  });
+  const {
+    isLoading: tvLoading,
+    data: tvData,
+    refetch: searchTv,
+  } = useQuery({
+    queryKey: ['searchTv', query],
+    queryFn: tvApi.search,
+    enabled: false,
+  });
+  const onChangeText = (text: string) => setQuery(text);
+  const onSubmit = () => {
+    if (query === '') {
+      return;
+    }
+    searchMovies();
+    searchTv();
+  };
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: colors.PRIMARY,
-      }}>
-      <Text style={{color: colors.TEXT}}>Search</Text>
-    </View>
+    <Container>
+      <SearchBar
+        placeholder="Search for Movie or TV Show"
+        placeholderTextColor="grey"
+        returnKeyType="search"
+        onChangeText={onChangeText}
+        onSubmitEditing={onSubmit}
+      />
+      {moviesLoading || tvLoading ? <Loader /> : null}
+      {moviesData ? (
+        <HList title="Movie Results" data={moviesData.results} />
+      ) : null}
+      {tvData ? <HList title="TV Results" data={tvData.results} /> : null}
+    </Container>
   );
 };
-
 export default Search;
