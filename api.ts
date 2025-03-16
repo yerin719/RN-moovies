351fbab74dbf828329992ec2aaf17722a5a8b9fc
erin <erin@techtaka.com>
2025-03-16 14:26:23 +0900

    TV screen

--- api.ts
+++ api.ts
@@ -18,6 +18,23 @@ export interface Movie {
   vote_count: number;
 }
 
+export interface TV {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
 interface BaseResponse {
   page: number;
   total_results: number;
@@ -28,19 +45,36 @@ export interface MovieResponse extends BaseResponse {
   results: Movie[];
 }
 
-const trending = () =>
-  fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`).then(res =>
-    res.json(),
-  );
+export interface TVResponse extends BaseResponse {
+  results: TV[];
+}
 
-const upcoming = () =>
-  fetch(
-    `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`,
-  ).then(res => res.json());
+export const moviesApi = {
+  trending: () =>
+    fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`).then(res =>
+      res.json(),
+    ),
+  upcoming: () =>
+    fetch(
+      `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`,
+    ).then(res => res.json()),
+  nowPlaying: () =>
+    fetch(
+      `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`,
+    ).then(res => res.json()),
+};
 
-const nowPlaying = () =>
-  fetch(
-    `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`,
-  ).then(res => res.json());
-
-export const moviesApi = {trending, upcoming, nowPlaying};
+export const tvApi = {
+  trending: () =>
+    fetch(`${BASE_URL}/trending/tv/week?api_key=${API_KEY}`).then(res =>
+      res.json(),
+    ),
+  airingToday: () =>
+    fetch(`${BASE_URL}/tv/airing_today?api_key=${API_KEY}`).then(res =>
+      res.json(),
+    ),
+  topRated: () =>
+    fetch(`${BASE_URL}/tv/top_rated?api_key=${API_KEY}`).then(res =>
+      res.json(),
+    ),
+};
--- screens/Tv.tsx
+++ screens/Tv.tsx
@@ -1,22 +1,58 @@
 import React from 'react';
-import {View, Text, useColorScheme} from 'react-native';
+import {RefreshControl, ScrollView, useColorScheme} from 'react-native';
+import {useQuery, useQueryClient} from '@tanstack/react-query';
+import {tvApi} from '../api';
+import HList from '../components/HList';
+import Loader from '../components/Loader';
 import {COLORS} from '../colors';
-
 const Tv = () => {
+  const queryClient = useQueryClient();
+  const {
+    isLoading: todayLoading,
+    data: todayData,
+    isRefetching: todayRefetching,
+  } = useQuery({
+    queryKey: ['tv', 'today'],
+    queryFn: tvApi.airingToday,
+  });
+  const {
+    isLoading: topLoading,
+    data: topData,
+    isRefetching: topRefetching,
+  } = useQuery({
+    queryKey: ['tv', 'top'],
+    queryFn: tvApi.topRated,
+  });
+  const {
+    isLoading: trendingLoading,
+    data: trendingData,
+    isRefetching: trendingRefetching,
+  } = useQuery({
+    queryKey: ['tv', 'trending'],
+    queryFn: tvApi.trending,
+  });
+  const onRefresh = () => {
+    queryClient.refetchQueries({queryKey: ['tv']});
+  };
+  const loading = todayLoading || topLoading || trendingLoading;
+  const refreshing = todayRefetching || topRefetching || trendingRefetching;
+  if (loading) {
+    return <Loader />;
+  }
   const isDark = useColorScheme() === 'dark';
   const colors = isDark ? COLORS.dark : COLORS.light;
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: colors.PRIMARY,
-      }}>
-      <Text style={{color: colors.TEXT}}>TV</Text>
-    </View>
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+      contentContainerStyle={{paddingVertical: 30}}
+      style={{backgroundColor: colors.PRIMARY}}>
+      <HList title="Trending TV" data={trendingData.results} />
+      <HList title="Airing Today" data={todayData.results} />
+      <HList title="Top Rated TV" data={topData.results} />
+    </ScrollView>
   );
 };
-
 export default Tv;
